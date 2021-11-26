d10e75f10b1e1f7c530ba656ef25a415d53e7928
Robert Edmonds <bobby.edmonds89@gmail.com>
2021-11-26 13:16:01 -0700

    commit tree e515be7478c3a553eee9fefc11d2a1f246890f51

--- index.js
+++ index.js
@@ -1,1 +1,5 @@
 // Write your code in this file!
+const currentUser = 'Robert Edmonds';
+const welcomeMessage = `Welcome to Flatbook, ${currentUser}!`;
+const excitedWelcomeMessage = (welcomeMessage.toUpperCase(currentUser));
+const shortGreeting = `Welcome, ${currentUser.substring(0,1)}!`;
